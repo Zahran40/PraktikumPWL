336a87bf1cac9c1da9188cd451ba9d3b9074c5b0
Zahran40 <araabbil@gmail.com>
2025-05-15 14:13:51 +0700

    Nama : Abbil Rizki Abdillah,Nim:241402033 ,(Nama Tabel yang saya buat adalah "Hari")

--- database/migrations/2025_05_08_035637_create_hari_table.php
+++ database/migrations/2025_05_08_035637_create_hari_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('hari', function (Blueprint $table) {
-            $table->id("id");
+            $table->id("id")->autoIncrement();
             $table->date("tanggal");
             $table->string("task",100);
         });
